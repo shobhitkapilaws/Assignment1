2cae2de446eac45a591eb1f55b0c551d6175ee31
shobhitkapilaws <68150131+shobhitkapilaws@users.noreply.github.com>
2020-07-12 16:02:06 +0530

    Update CalculatorTest.java

--- src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
+++ src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
@@ -10,18 +10,6 @@ public class CalculatorTest {
         final long result = new Calculator().addFucn(2, 3);
         assertThat(result, is(5L));
     }
-
-    @Test
-    public void threeMinusTwoIsOne() throws Exception {
-        final long result = new Calculator().subFucn(2, 3);
-        assertThat(result, is(1L));
-    }
-    
-    @Test
-    public void threeXThreeIsNine() throws Exception {
-        final long result = new Calculator().mulFucn(3, 3);
-        assertThat(result, is(9L));
-    }
    
 }
 
